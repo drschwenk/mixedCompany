eaa5aad3f56c0474c62fd9851d76fae57153880a
drschwenk <schwgit2@gmail.com>
2016-01-02 12:50:25 -0800

    about to try new approach to resolving dupes

--- parser.py
+++ parser.py
@@ -2,7 +2,7 @@ import cPickle as pickle
 import re
 from collections import defaultdict, deque
 
-def flavor_ingredient_mapper(raw_text, look_ahead_range):
+def flavor_ingredient_mapper(raw_text):
     """
     Creates a chemical compound to ingredient mapping from fenaroli's flavors. The ingredients are not individually
     extracted here, and instead returned as a single combined string
@@ -10,56 +10,45 @@ def flavor_ingredient_mapper(raw_text, look_ahead_range):
     :return: dictionary mapping compound:ingredient list
     """
 
-    def check_for_header(new_comp, text_blobs, look_ahead_index, target_pattern):
-        """
-        Searches for compound appearing in a header, which would produce a duplicate entry if not caught
-        :param new_comp: compound being evaluated
-        :param text_blobs:
-        :param look_ahead_index:
-        :param target_pattern:
-        :return:
-        """
-        for j in range(look_ahead_index, look_ahead_index + look_ahead_range):
-            try:
-                r1 = re.findall(target_pattern, text_blobs[j])
-                if new_comp == re.findall(target_pattern, text_blobs[j]):
-                    # a potential match has been found
-                    return True
-            except KeyError:
-                pass
-        return False
-
     flavor_dict = defaultdict(list)
-    ignore_terms = ['EINECS No :']
+    ignore_terms = ['EINECS No :', 'EINECS No ']
     re.MULTILINE
-    ingredient_pattern = re.compile('Natural occurrence:(.+)')
+    ingredient_pattern = re.compile('Natural\s+occurrence:(.+)')
     compound_pattern = re.compile('(.*[A-Z]{6,20}.*)')
 
     blobs = raw_text.split('\n\n')
     comp_q = deque([], maxlen=3)
-    for i in range(int(len(blobs)*0.2), len(blobs)):
+    ing_q = deque([], maxlen=3)
+    comp_list = []
+    for i in range(int(len(blobs)*0.3), int(len(blobs)*0.6)):
         text = blobs[i].replace('\n', '')
-        ing = re.findall(ingredient_pattern, text)
-        comp = re.findall(compound_pattern, text)
-        if comp and comp[0] not in ignore_terms:
-            if not check_for_header(comp, blobs, i, compound_pattern):
-                comp_q.append(comp[0])
-        if ing:
+        potential_ing = re.findall(ingredient_pattern, text)
+        potential_comp = re.findall(compound_pattern, text)
+
+        if potential_comp and potential_comp[0] not in ignore_terms:
+            tt1 = potential_comp[0].strip()
+            comp_q.append(potential_comp[0].strip())
+
+        if potential_ing:
+            ing_q.append(potential_ing)
             try:
-                flavor_dict[comp_q.pop().strip()] = ing[0].strip()
+                new_comp = comp_q.pop()
+                if new_comp in comp_q:
+                    flavor_dict[comp_q[-2]] = flavor_dict[comp_q[-1]]
+                else:
+                    comp_q.append(new_comp)
+                    flavor_dict[new_comp] = potential_ing[0].strip()
             except IndexError:
                 pass
 
-    remove_terms = ['Not reported found in nature','n/a','Reported found in nature',
+    remove_terms = ['Not reported found in nature', 'n/a', 'Reported found in nature',
                     'Data not found', 'Reported not found in nature', 'No data found ']
-    flav_comp_dict = {k:v for k,v in flavor_dict.iteritems() if v not in remove_terms}
-    return flav_comp_dict
+    flav_comp_dict = {k: v for k, v in flavor_dict.iteritems() if v not in remove_terms}
+    return comp_list, flav_comp_dict
 
+if __name__ == '__main__':
+    with open('../../data/doc_joined.pkl', 'r') as f:
+        joined = pickle.load(f)
 
-with open('../../data/doc_joined.pkl', 'r') as f:
-    joined = pickle.load(f)
-
-cleaned_joined = joined.replace('.', ' ')
-fcd = flavor_ingredient_mapper(cleaned_joined, 50)
-
-print(fcd['2-HEXENAL'])
+    cleaned_joined = joined.replace('.', ' ')
+    fcd = flavor_ingredient_mapper(cleaned_joined)
