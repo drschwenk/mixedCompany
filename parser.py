39a41dd42a53d38e9a504046424df458b1dc0868
drschwenk <schwgit2@gmail.com>
2016-01-02 14:30:30 -0800

    Working parser with test suite

--- parser.py
+++ parser.py
@@ -9,46 +9,43 @@ def flavor_ingredient_mapper(raw_text):
     :param raw_text: single string containing entire doc
     :return: dictionary mapping compound:ingredient list
     """
-
-    flavor_dict = defaultdict(list)
     ignore_terms = ['EINECS No :', 'EINECS No ']
-    re.MULTILINE
+    # These must be removed here to preserve the queue ordering
     ingredient_pattern = re.compile('Natural\s+occurrence:(.+)')
     compound_pattern = re.compile('(.*[A-Z]{6,20}.*)')
-
     blobs = raw_text.split('\n\n')
+
+    flavor_dict = defaultdict(list)
     comp_q = deque([], maxlen=3)
-    ing_q = deque([], maxlen=3)
-    comp_list = []
-    for i in range(int(len(blobs)*0.3), int(len(blobs)*0.6)):
-        text = blobs[i].replace('\n', '')
+    ing_q = deque([], maxlen=2)
+    for blob in blobs:
+        text = blob.replace('\n', '')
         potential_ing = re.findall(ingredient_pattern, text)
         potential_comp = re.findall(compound_pattern, text)
 
         if potential_comp and potential_comp[0] not in ignore_terms:
-            tt1 = potential_comp[0].strip()
             comp_q.append(potential_comp[0].strip())
-
         if potential_ing:
-            ing_q.append(potential_ing)
+            ing_q.append(potential_ing[0].strip())
             try:
-                new_comp = comp_q.pop()
-                if new_comp in comp_q:
-                    flavor_dict[comp_q[-2]] = flavor_dict[comp_q[-1]]
+                c1 = comp_q[0]
+                c2 = comp_q[1]
+                c3 = comp_q[2]
+                # This is to check for page headers in the compound queue
+                if c2 == c1:
+                    flavor_dict[c2] = ing_q[-1]
                 else:
-                    comp_q.append(new_comp)
-                    flavor_dict[new_comp] = potential_ing[0].strip()
+                    flavor_dict[c3] = ing_q[-1]
             except IndexError:
                 pass
-
+    # Removing the compounds not found in nature
     remove_terms = ['Not reported found in nature', 'n/a', 'Reported found in nature',
                     'Data not found', 'Reported not found in nature', 'No data found ']
     flav_comp_dict = {k: v for k, v in flavor_dict.iteritems() if v not in remove_terms}
-    return comp_list, flav_comp_dict
+    return flav_comp_dict
 
 if __name__ == '__main__':
     with open('../../data/doc_joined.pkl', 'r') as f:
         joined = pickle.load(f)
-
     cleaned_joined = joined.replace('.', ' ')
     fcd = flavor_ingredient_mapper(cleaned_joined)
